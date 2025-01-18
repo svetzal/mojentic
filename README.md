f1968a93968a739473840c4fa01bbfa48f1da35e
Stacey Vetzal <stacey@vetzal.com>
2025-01-17 19:14:45 -0500

    more moving things around

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mojentic
+
+Mojentic is an agentic framework that aims to provide a simple and flexible way to assemble teams of agents to solve
+complex problems. Design goals are to be asynchronous with a pubsub messaging architecture.
--- mojentic/__init__.py
+++ mojentic/__init__.py
@@ -4,8 +4,6 @@ complex problems. Design goals are to be asynchronous with a pubsub messaging ar
 """
 import importlib.metadata as _importlib_metadata
 
-from mojentic.base_llm_agent import BaseLLMAgent
-
 __version__: str
 try:
     __version__ = _importlib_metadata.version(__name__)
