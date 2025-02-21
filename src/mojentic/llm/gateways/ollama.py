e13d6d876ae57a3687f9d88ed5402cee84cfebc6
Stacey Vetzal <stacey@vetzal.com>
2025-02-21 07:20:07 -0500

    blocked on implementing streaming

--- src/mojentic/llm/gateways/ollama.py
+++ src/mojentic/llm/gateways/ollama.py
@@ -1,7 +1,7 @@
-from typing import List
-
+from typing import List, Iterator
 import structlog
 from ollama import Client, Options, ChatResponse
+from pydantic import BaseModel
 
 from mojentic.llm.gateways.llm_gateway import LLMGateway
 from mojentic.llm.gateways.models import LLMToolCall, LLMGatewayResponse
@@ -9,6 +9,9 @@ from mojentic.llm.gateways.ollama_messages_adapter import adapt_messages_to_olla
 
 logger = structlog.get_logger()
 
+class StreamingResponse(BaseModel):
+    """Simple wrapper for streaming response content"""
+    content: str
 
 class OllamaGateway(LLMGateway):
     """
@@ -100,5 +103,64 @@ class OllamaGateway(LLMGateway):
             tool_calls=tool_calls,
         )
 
+    def complete_stream(self, **args) -> Iterator[StreamingResponse]:
+        """
+        Stream the LLM response from Ollama service.
+
+        Parameters
+        ----------
+        model : str
+            The name of the model to use, as appears in `ollama list`.
+        messages : List[LLMMessage]
+            A list of messages to send to the LLM.
+        tools : Optional[List[LLMTool]]
+            A list of tools to use with the LLM. If a tool call is requested, the tool will be called and the output
+            will be included in the response.
+        temperature : float, optional
+            The temperature to use for the response. Defaults to 1.0.
+        num_ctx : int, optional
+            The number of context tokens to use. Defaults to 32768.
+        num_predict : int, optional
+            The number of tokens to predict. Defaults to no limit.
+
+        Returns
+        -------
+        Iterator[StreamingResponse]
+            An iterator of StreamingResponse objects containing response chunks.
+        """
+        logger.info("Delegating to Ollama for streaming completion", **args)
+
+        options = self._extract_options_from_args(args)
+        ollama_args = {
+            'model': args['model'],
+            'messages': adapt_messages_to_ollama(args['messages']),
+            'options': options,
+            'stream': True
+        }
+
+        #
+        # This is here 2025-02-21 to demonstrate a deficiency in Ollama tool calling
+        # using the Stream option. We can't get chunk by chunk responses from the LLM
+        # when using tools. This limits our ability to explore streaming capabilities
+        # in the mojentic API, so I'm pausing this work for now until this is resolved.
+        # https://github.com/ollama/ollama/issues/7886
+        #
+
+        # if 'tools' in args and args['tools'] is not None:
+        #     ollama_args['tools'] = [t.descriptor for t in args['tools']]
+
+        stream = self.client.chat(**ollama_args)
+
+        for chunk in stream:
+            if chunk.message:
+                if chunk.message.content:
+                    yield StreamingResponse(content=chunk.message.content)
+                # if chunk.message.tool_calls:
+                #     for tool_call in chunk.message.tool_calls:
+                #         yield StreamingResponse(
+                #             content=f"\nTOOL CALL: {tool_call.function.name}({tool_call.function.arguments})\n"
+                #         )
+
     def get_available_models(self) -> List[str]:
         return [m.model for m in self.client.list().models]
+
